e9584c8ea089c758f62acc4a72d85ad390bbc744
Horse1jj <foste1jj.seth@gmail.com>
2025-04-21 18:11:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from discord.ext import commands
+from discord import Intents, app_commands
 from modules import config
 from os import getcwd, chdir, path, listdir
 from pathlib import Path
 
-bot = commands.Bot(command_prefix=config.Config().botPrefix)
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=config.Config().botPrefix,
+            intents=Intents.all()
+        )
+        self.config = config.Config()
+
+    async def setup_hook(self):
+        chdir(path.dirname(__file__))
+        chdir('cogs')
+        for dir in listdir(Path(getcwd())):
+            if dir.endswith('.py'):
+                self.load_extension(f'cogs.{dir[:-3]}')
 
 
-if __name__ == '__main__':
-    chdir(path.dirname(__file__))
-    chdir('cogs')
-    for dir in listdir(Path(getcwd())):
-        if dir.endswith('.py'):
-            bot.load_extension(f'cogs.{dir[:-3]}')
+        guild = discord.Object(id=int(self.config.mainserverid))  
+        await self.tree.sync(guild=guild)
+        print(f"Synced commands to guild {guild.id}")
 
-bot.run(config.Config().botToken)
+bot = MyBot()
+bot.run(bot.config.botToken)
